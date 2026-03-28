999020e59f216ac42d4dddd0c0226f91f7293e16
Reza <muh.reza2441@gmail.com>
2026-03-28 22:35:17 +0800

    Refactor: cleaning dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "app/api/**": [
-      "node_modules/@sparticuz/chromium/**",
-      "public/fonts/**",
-    ],
+    "app/api/**": ["node_modules/@sparticuz/chromium/**", "public/fonts/**"],
   },
   turbopack: {},
 };
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({});
 
 export default nextConfig;
